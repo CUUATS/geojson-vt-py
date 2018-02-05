95aca02393fc5f875360ecd5b1ee1c7f47e6339c
Matt Yoder <myoder@ccrpc.org>
2018-02-05 08:56:02 -0600

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+description = 'A Python port of JS GeoJSON-VT for generating vector map tiles.'
+long_description = description
+
+if os.path.exists('README.md'):
+    try:
+        # Use pypandoc to convert the Markdown readme to reStructuredText.
+        # See: http://johnmacfarlane.net/pandoc/installing.html
+        from pypandoc import convert
+        long_description = convert('README.md', 'rst', format='md')
+    except ImportError:
+        pass
+
+setup(
+    name='geojsonvt',
+    version='0.1',
+    packages=find_packages(exclude=['tests']),
+    author='Matt Yoder',
+    author_email='myoder@ccrpc.org',
+    description=description,
+    long_description=long_description,
+    license='ISF',
+    keywords='geojson vector map tile',
+    url='https://github.com/CUUATS/geojson-vt-py',
+)
